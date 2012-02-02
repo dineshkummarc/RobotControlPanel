510feaa585ef43d3fc29a1ed7d5d2a1e763bfe80
Boldizsar Kiss <boldi.kiss@gmail.com>
2012-02-02 19:14:42 +0100

    Radar

---
+++ Radar/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApplication23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.radarScreen = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.radarScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radarScreen
+            // 
+            this.radarScreen.Location = new System.Drawing.Point(12, 12);
+            this.radarScreen.Name = "radarScreen";
+            this.radarScreen.Size = new System.Drawing.Size(800, 500);
+            this.radarScreen.TabIndex = 0;
+            this.radarScreen.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(826, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Etwas lofasz";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(829, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(857, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Step";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(857, 445);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start/Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 518);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radarScreen);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radarScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox radarScreen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Radar/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication23
+{
+    public partial class Form1 : Form
+    {
+        Bitmap radarScreenBmp;
+        Graphics radarGraphic;
+        Pen darkGreenPen;
+        Pen blackPen;
+        const int BASELINE = 400;
+        const int WIDTH = 800;
+        const int HEIGHT = 500;
+        const int DIVSTEP = 8;
+        const int MAXDIV = 500;
+        int fi=10;
+        int dir = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radarScreenBmp = new Bitmap(WIDTH, HEIGHT);
+            radarScreen.Image = radarScreenBmp;
+            radarGraphic = Graphics.FromImage(radarScreenBmp);
+            radarGraphic.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            darkGreenPen = new Pen(Brushes.DarkGreen);
+            blackPen = new Pen(Brushes.Black);
+            drawDiv();
+            radarGraphic.FillRectangle(Brushes.Black, 0, BASELINE, WIDTH, HEIGHT - BASELINE);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (fi > 180 || fi < 10) dir *= -1; 
+            fi += dir * 2;
+
+            drawDiv();
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddPie((WIDTH - (MAXDIV)) / 2, BASELINE - (((MAXDIV)) / 2), (MAXDIV), (MAXDIV), -fi, 10);
+            PathGradientBrush brush = new PathGradientBrush(path);
+            brush.CenterPoint = new PointF(WIDTH-(WIDTH / 2 - (int)(Math.Cos(Deg2Rad((dir>0)?fi:fi-10)) * (MAXDIV / 2))), BASELINE - (int)(Math.Sin(Deg2Rad((dir>0)?fi:fi-10)) * (MAXDIV / 2)));
+            brush.SurroundColors = new Color[] { Color.FromArgb(0, 0, 255, 0) };
+            brush.CenterColor = Color.FromArgb(255, 0, 255, 0);
+            radarGraphic.FillPath(brush, path);
+
+            radarGraphic.FillRectangle(Brushes.Black, 0, BASELINE, WIDTH, HEIGHT - BASELINE);
+            radarScreen.Invalidate();
+            path.Dispose();
+            brush.Dispose();
+        }
+
+        private double Deg2Rad(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        private void drawDiv()
+        {
+            radarGraphic.Clear(Color.Black);
+            radarGraphic.DrawLine(darkGreenPen, (WIDTH - MAXDIV) / 2, BASELINE, WIDTH - ((WIDTH - MAXDIV) / 2), BASELINE);
+            for (int i = 1; i < DIVSTEP + 1; i++) radarGraphic.DrawEllipse(darkGreenPen, ((WIDTH - (MAXDIV / DIVSTEP) * i) / 2), BASELINE - (((MAXDIV / DIVSTEP) * i) / 2), (MAXDIV / DIVSTEP) * i, (MAXDIV / DIVSTEP) * i);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1_Tick(null, null);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+    }
+}
